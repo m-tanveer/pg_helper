544990afabe6ec01addfe4e4329388e13f7256f2
mohamed.t <mohamedt@zeta.tech>
2020-05-11 00:31:32 +0530

    .

--- copy.sql
+++
@@ -1,4 +0,0 @@
-
-
-
-\copy (select  From foo) To '/tmp/test.csv' With CSV DELIMITER ',';
--- default_privileges.sql
+++ default_privileges.sql
@@ -2,16 +2,16 @@
 -- grant permissions to the application user in the public schema
 --
 
-ALTER DEFAULT PRIVILEGES IN SCHEMA public FOR ROLE master_prod GRANT ALL on TABLES TO anonuser ;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public FOR ROLE master_prod GRANT ALL on SEQUENCES TO anonuser ;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public FOR ROLE master_prod GRANT ALL on FUNCTIONS TO anonuser ;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public FOR ROLE master_user GRANT ALL on TABLES TO admindb ;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public FOR ROLE master_user GRANT ALL on SEQUENCES TO admindb ;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public FOR ROLE master_user GRANT ALL on FUNCTIONS TO admindb ;
 
 --
 -- revoke default privileges
 --
 
-ALTER DEFAULT PRIVILEGES IN SCHEMA public FOR ROLE master_prod REVOKE ALL on TABLES FROM anonuser ;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public FOR ROLE master_prod REVOKE ALL on SEQUENCES FROM anonuser ;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public FOR ROLE master_prod REVOKE ALL on FUNCTIONS FROM anonuser ;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public FOR ROLE master_user REVOKE ALL on TABLES FROM admindb ;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public FOR ROLE master_user REVOKE ALL on SEQUENCES FROM admindb ;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public FOR ROLE master_user REVOKE ALL on FUNCTIONS FROM admindb ;
 
--- end of script 
+-- end of script
